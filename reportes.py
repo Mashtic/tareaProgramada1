b2242f6d39952cdd57e891067110b2c4dadd7b69
Ian Coto <iancotosoto@gmail.com>
2022-11-02 23:54:32 -0600

    Eliminan html y más funciones a reportes

--- reportes.py
+++ reportes.py
@@ -7,7 +7,10 @@
 visitantes = [[305430092, ("Ian", "Coto", "Soto"), [], [], False], [987654321, ("Pedro", "Moto", "Zote"), [], [], True], 
 [696969699, ("Esteban", "Mi", "Novio"), [], [], False]]
 
-# Función auxiliar
+# Función 
+def obtenerNombre(pVisitante):
+    return pVisitante[1][0] + " " + pVisitante[1][1] + " " + pVisitante[1][2]
+
 def crearArchivoHtml(pNombre, pInfo):
     archivo = open(pNombre + ".html", 'w')
     archivo.write(pInfo)
@@ -16,7 +19,15 @@ def crearArchivoHtml(pNombre, pInfo):
 
 # 7. Reportes
 
-# Perfil visitante
+# Perfil 
+
+def reporteVisitante(pVisitante):
+    cedula = str(pVisitante[0])
+    htmlVisit = ("<html>\n<head>\n<title> \nReporte " + cedula +
+    "</title>\n</head> <body><h2>Reporte visitante" + "</h2>"
+    "<h3>Cedula</h3> \n<p>"+ cedula +"</p>"
+    "<h3>Nombre</h3> \n<p>"+str(obtenerNombre(pVisitante))+"</p>""\n</body></html>")
+    return crearArchivoHtml("Reporte " + cedula, htmlVisit)
 
 # Estadísticas astrónomos
 
@@ -33,17 +44,17 @@ def obtenerVisitBaja(pVisitantes):
             visitBaja.append(visitante)
     return visitBaja
 
-def reporteVisitBaja():
+def reporteVisitBaja(pVisitantes):
     strTabla = "<html>\n<head>\n<title> \nBaja visitantes \n\
                 </title>\n</head><body><h1>Visitantes dados de baja</h1> \
                 <table><tr><th>Cédula</th><th>Nombres</th></tr>"
-    visitBaja = obtenerVisitBaja(visitantes)
+    visitBaja = obtenerVisitBaja(pVisitantes)
     for visitante in visitBaja:
-        strElementos = ("<tr><td>"+str(visitante[0])+"</td><td>"+ visitante[1][0]+" "+
-        visitante[1][1]+" "+visitante[1][2]+"</td></tr>")
+        strElementos = ("<tr><td>"+str(visitante[0])+"</td><td>"+ obtenerNombre(visitante)+"</td></tr>")
         strTabla += strElementos
     strTabla += "</table></html>"
     return crearArchivoHtml("Bajas visitantes", strTabla)
 
 # Recursos tipo
-
+for visitante in visitantes:
+    reporteVisitante(visitante)
